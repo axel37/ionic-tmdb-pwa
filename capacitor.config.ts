5e843d0bb7f770ec943739b159733903b8d5aeee
Axel Choteau <axelc37@outlook.fr>
2024-05-13 13:54:28 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'supinfo-4HYBD-tp2-tmdb',
+  webDir: 'dist'
+};
+
+export default config;

4eb0a2948957f7cec53f5b9766114b7a027c394a
Axel Choteau <axelc37@outlook.fr>
2024-05-13 14:36:16 +0200

    TMDB credit notice

---
+++ src/components/TmdbCredits/TmdbCredits.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TmdbCredits: React.FunctionComponent = () => {
+    return (
+        <p>This product uses the <a href="https://www.themoviedb.org">TMDB</a> API but is not endorsed or certified by TMDB.</p>
+    );
+};
+
+export default TmdbCredits;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,27 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import TmdbCredits from "../components/TmdbCredits/TmdbCredits";
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Blank</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Blank</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <ExploreContainer/>
+                <TmdbCredits/>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;

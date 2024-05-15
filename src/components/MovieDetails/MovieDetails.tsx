f287a3bf51d96435b2710852b4eb1d4027645498
Axel Choteau <axelc37@outlook.fr>
2024-05-15 16:39:44 +0200

    Movie details : Poster modal

--- src/components/MovieDetails/MovieDetails.tsx
+++ src/components/MovieDetails/MovieDetails.tsx
@@ -1,21 +1,51 @@
 // src/components/MovieDetails/MovieDetails.tsx
 import {MovieInterface} from "../../domain/MovieInterface";
-import {IonNote} from "@ionic/react";
+import {
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonImg,
+    IonModal,
+    IonNote,
+    IonTitle,
+    IonToolbar
+} from "@ionic/react";
+import {useRef} from "react";
 
 interface Props {
     movie: MovieInterface
 }
 
 export default function MovieDetails({movie}: Props): JSX.Element {
+    const modal = useRef<HTMLIonModalElement>(null);
+
     return (
         <article>
             <h1>{movie.title}</h1>
             <IonNote>{movie.release_date}</IonNote>
             <br/>
-            <img src={movie.poster_url_large} alt="The movie's poster."/>
+            <IonButton id="open-modal" expand="block">
+                {/* TODO : Make poster smaller. It must be obvious that it can be clicked ! */}
+                <img src={movie.poster_url_small} alt="The movie's poster."/>
+            </IonButton>
             <p>{movie.overview}</p>
             <p>Vote average: {movie.vote_average}</p>
             <p>Vote count: {movie.vote_count}</p>
+
+            <IonModal ref={modal} trigger="open-modal"  style={{"--height": "100%"}}>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonButtons slot="start">
+                            <IonButton onClick={() => modal.current?.dismiss()}>Close</IonButton>
+                        </IonButtons>
+                        <IonTitle>Poster</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className="ion-padding">
+                    <IonImg src={movie.poster_url_large} alt="The movie's poster."/>
+                </IonContent>
+            </IonModal>
         </article>
     );
 }
